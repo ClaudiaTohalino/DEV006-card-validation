196667e5111c7b88e9d2df100844141133bcc3d7
Claudia Tohalino <claudia.tohalino@gmail.com>
2023-03-30 08:11:24 -0500

    nuevos cambios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,118 @@
 import validator from './validator.js';
 
+//Con esto estoy estoy definiendo que tengo que acceder primero al formulario con querySelector
+const formularioTarjeta = document.querySelector('#formularioTarjeta');
+
+//Con esto esoty definiendo que tengo que acceder primero al numero y luego al nombre de la forma tarjeta
+const formaTarjetaNumero= document.querySelector('#allFormaTarjeta .numeroFormaTarjeta');
+const formaTarjetaNombre= document.querySelector('#allFormaTarjeta .nombreFormaTarjeta');
+
+//Esto de aqui no tiene mucho sentido a mi parecer, si se puede busca mas información
+//const numerotarjeta = document.querySelector('#numeroTarjeta');
+
+
+//Para voltear la tarjeta
+//const mostrarFrente =() => {
+//  if (formularioTarjeta.classList.contains('active')){
+//    formularioTarjeta.classList.remove('active')
+//  }
+//}
+
+formularioTarjeta.numeroFormularioTarjeta.addEventListener ('keyup', (e) => {
+  //Este console.log es para ver la impresión de e en la consola
+  //console.log(e.target.value)
+  //Esta variable me guarda el valor de formulario.numeroTarjeta
+  const valorNumeroFormularioTarjeta= e.target.value;
+
+  //vamos a eliminar todo lo que no sea numero del input de la tarjeta
+  formularioTarjeta.numeroFormularioTarjeta.value=valorNumeroFormularioTarjeta
+    //Para los espacios en blanco expresion regular \s
+    .replace(/\s/g, '')
+    //Para las letras expresion regular \D
+    .replace(/\D/g, '')
+    //Para poner un bonito espacio cada 4 numeros y se vea como de tarjeta
+    //.replace(/([0-9]{4})/g, '$1 ')
+    //Para que el último espacio se borre 
+    .trim();
+
+  formaTarjetaNumero.textContent=valorNumeroFormularioTarjeta;
+  if (valorNumeroFormularioTarjeta === ""){
+    formaTarjetaNumero.textContent= '#### #### #### ####';
+  }else{
+    const ValorPrueba = valorNumeroFormularioTarjeta2.value;
+    const maskifyVariable = validator.maskify(ValorPrueba);
+    //console.log(maskifyVariable);
+    formaTarjetaNumero.textContent= maskifyVariable
+    //console.log(formaTarjetaNumero.textContent);
+  }
+   
+});
+    
+
+formularioTarjeta.nombreFormularioTarjeta.addEventListener('keyup', (e) => {
+  const valorNombreFormularioTarjeta = e.target.value;
+  formularioTarjeta.nombreFormularioTarjeta.value = valorNombreFormularioTarjeta
+    .replace(/[0-9]/g, '');
+  formaTarjetaNombre.textContent = valorNombreFormularioTarjeta;
+  if(valorNombreFormularioTarjeta === ""){
+    formaTarjetaNombre.textContent = 'Jhon Doe';
+  }
+    
+});
+
+//formularioTarjeta.mesFormularioTarjeta.addEventListener('keyup', (e) => {
+//    let valorMesFormularioTarjeta = e.target.value;
+//    formularioTarjeta.mesFormularioTarjeta.value = valorMesFormularioTarjeta
+//    .replace(/[0-9]/g, '');
+//    formaTarjetaNombre.textContent = valorMesFormularioTarjeta;
+//    if(valorMesFormularioTarjeta == ""){
+//        formaTarjetaNombre.textContent = 'MM';
+//    }
+    
+//});
+
+
+
+
+const botonValor=document.getElementById('botonPagar');
+const valorNumeroFormularioTarjeta2 =document.getElementById('numeroFormularioTarjeta');
+const valorNombreFormularioTarjeta2 =document.getElementById('nombreFormularioTarjeta');
+const valorCvvFormularioTarjeta2 = document.getElementById('cvvFormularioTarjeta')
+
+botonValor.addEventListener('click', function boton() {
+  
+  const valorPrueba = valorNumeroFormularioTarjeta2.value;
+  const valorNombre = valorNombreFormularioTarjeta2.value;
+  const valorCVV = valorCvvFormularioTarjeta2.value;
+    
+  //console.log(valorPrueba);
+  
+  
+  if (valorPrueba==="" && valorNombre!=="" && valorCVV!=="") {
+    alert("Por favor introducir el numero de la tarjeta"); 
+  }else if (valorPrueba!=="" && valorNombre==="" && valorCVV!==""){
+    alert("Por favor introducir el nombre de titular de la tarjeta");
+  }else if (valorPrueba!=="" && valorNombre!=="" && valorCVV===""){
+    alert("Por favor introducir el CVV de la tarjeta");
+  }else if (valorPrueba==="" && valorNombre==="" && valorCVV!==""){
+    alert("Por favor introducir el numero y el nombre de titular de la tarjeta");
+  }else if (valorPrueba==="" && valorNombre!=="" && valorCVV===""){
+    alert("Por favor introducir el numero y el CVV de la tarjeta");
+  }else if (valorPrueba!=="" && valorNombre==="" && valorCVV===""){
+    alert("Por favor introducir el nombre y el CVV de la tarjeta");
+  }else if (valorPrueba==="" && valorNombre==="" && valorCVV===""){
+    alert("Por favor introducir el numero, el nombre de titular y el CVV de la tarjeta");
+  }else if (valorPrueba!=="" && valorNombre!=="" && valorCVV!=="" && validator.isValid(valorPrueba)=== true){
+    alert("Su pago esta siendo procesado");
+  }else if (valorPrueba!=="" && valorNombre!=="" && valorCVV!=="" && validator.isValid(valorPrueba)=== false){ 
+    alert("La tarjeta no es válida, introduzca otra tarjeta");
+  }
+// if (validator.isValid(valorPrueba)=== true && valorPrueba !== ""){
+//   alert("Su pago esta siendo procesado");
+// }else if (validator.isValid(valorPrueba)===false){
+//   alert("La tarjeta no es válida, introduzca otra tarjeta");
+});
+
+    
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,54 @@
 const validator = {
-  // ...
-};
+
+  isValid: function(valorPrueba) {
+    //Aqui estoy conviertiendo el numero de la tarjeta en un string porque el split solo funciona con string y es el unico metodo que me se para separarlos digitos
+    const cambioAString= valorPrueba.toString();
+    //Aplico split para separar los digitos del string y aprovecho de darles reversa
+    const separadoString= cambioAString.split('').reverse();
+    //console.log(separadoString);
+    //voy a hacer un for para poder multiplicar los digitos pares x2 
+    //el for tiene que hacer que me tome los numeros pares
+    for (let i = 1; i < separadoString.length; i += 2) {
+      separadoString[i] = separadoString[i] * 2;
+    //console.log(separadoString);
+    }
+    
+    //ya tengo los numeros multiplicados por 2, ahora quiero sumar los digitos 
+    //defino que suma empiece en 0 para despues sumarle los digitos a suma
+    let suma=0;
+    //con un for, voy a decirle que quiero que recorra todos los numeros, a todos los numeros mayores de 10 les saque el residuo de dividirlos entre 10 y le sume el 1 que es del 10
+    for (let i =0; i <separadoString.length; i++){
+    //Primero voya a encontrar las posiciones del array donde el numero es mayor a 10 con este if
+      if(separadoString[i]>=10){
+      //si imprimo este console log me va a dar las posiciones del array donde i>10
+        separadoString[i] = separadoString[i] - 9;
+      }
+            
+      suma+= parseInt(separadoString[i]);
+    }
+    if(suma % 10 === 0){
+      return true;
+    }else{
+      return false;
+    }
+  },
+    
+  maskify: function(valorPrueba){
+
+    const cambioAStringMaskify = valorPrueba.toString();
+    let vacio= "";
+    for(let i=0; i<cambioAStringMaskify.length; i++){
+      if(i >= cambioAStringMaskify.length - 4 ){
+        vacio= vacio + cambioAStringMaskify[i]
+      }else{
+        vacio = vacio + "#"
+      }
+    }
+    return vacio;
+  
+    
+    
+  }
+}
 
 export default validator;
